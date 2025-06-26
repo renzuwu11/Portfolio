b67f21d90db925594df5f57e0ecdcd741e9017cc
thatsme-espresso <Fababeir.renzpaul.12202003@gmail.com>
2025-06-26 21:01:17 +0800

    updated port

--- portfolio.py
+++ portfolio.py
@@ -58,4 +58,5 @@ def contact():
     return render_template("contact.html", page="contact")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
